8190bec2f032403c01ae591deb15df2889b978b0
Sarunas Juodagalvis <143128406+sjuoda@users.noreply.github.com>
2025-02-10 18:05:28 -0700

    ok

--- Assets/Script/MenuButtons.cs
+++ Assets/Script/MenuButtons.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public void LoadButton()
+    public void LoadButton(string sceneName)
     {
-        SceneManager.LoadScene("SampleScene")
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitApp()
